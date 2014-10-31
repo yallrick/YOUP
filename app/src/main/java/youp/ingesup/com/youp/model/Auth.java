e97820b624bb7821e4490d26b8dc927aa435f005
dmoro33 <moro.damien@gmail.com>
2014-10-31 15:00:32 +0100

    [FM13] Login - Damien Singleton d'authentification.

---
+++ app/src/main/java/youp/ingesup/com/youp/model/Auth.java
@@ -0,0 +1,16 @@
+package youp.ingesup.com.youp.model;
+
+/**
+ * Created by Damiano on 31/10/2014.
+ */
+public class Auth {
+
+    public Auth instance;
+    public User
+
+    private Auth()
+    {
+
+    }
+
+}
---
+++ app/src/main/java/youp/ingesup/com/youp/model/User.java
@@ -0,0 +1,33 @@
+package youp.ingesup.com.youp.model;
+
+/**
+ * Created by Damiano on 31/10/2014.
+ */
+public class User {
+
+    private int id;
+    private String nom;
+
+    public User(int _id, String _nom)
+    {
+        this.id = _id;
+        this.nom = _nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int _id) {
+        this.id = _id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String _nom) {
+        this.nom = _nom;
+    }
+
+}

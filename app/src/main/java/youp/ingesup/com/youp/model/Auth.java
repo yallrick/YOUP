8f8181cbbc818c52f725e5369dc3ce98543c26f3
dmoro33 <moro.damien@gmail.com>
2014-10-31 16:08:37 +0100

    [FM13] Login - Damien Singleton d'authentification.

--- app/src/main/java/youp/ingesup/com/youp/model/Auth.java
+++ app/src/main/java/youp/ingesup/com/youp/model/Auth.java
@@ -5,12 +5,31 @@ package youp.ingesup.com.youp.model;
  */
 public class Auth {
 
-    public Auth instance;
+    private static Auth instance = null;;
+
+
     private User user;
+    private String token;
 
-    private Auth()
+    private Auth(User _user, String _token)
     {
-
+        this.user = _user;
+        this.token = _token;
     }
 
+    public static Auth getInstance(User _user, String _token) {
+        if(instance == null) {
+            instance = new Auth(_user, _token);
+        }
+        return instance;
+    }
+
+    public User getUser()
+    {
+        return this.user;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }

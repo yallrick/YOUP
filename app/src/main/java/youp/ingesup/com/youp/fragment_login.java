f0b509d8bb5865f8e273434ef77a97f33852fd03
dmoro33 <moro.damien@gmail.com>
2014-10-31 16:08:37 +0100

    [FM13] Login - Damien Intégration écran. (cherry picked from commit 8f8181c)

--- app/src/main/java/youp/ingesup/com/youp/fragment_login.java
+++ app/src/main/java/youp/ingesup/com/youp/fragment_login.java
@@ -7,72 +7,64 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import javax.xml.datatype.Duration;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link fragment_login.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link fragment_login#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class fragment_login extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_login.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_login newInstance(String param1, String param2) {
-        fragment_login fragment = new fragment_login();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Button btLogin;
+    private Button btLostPassword;
+
+    private EditText etUsername;
+    private EditText etPassword;
+
     public fragment_login() {
-        // Required empty public constructor
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+
+        /* Récupération des éléments de la vue */
+        btLogin = (Button)root.findViewById(R.id.bt_Login);
+        btLostPassword = (Button) root.findViewById(R.id.btLostPassword);
+        etPassword = (EditText) root.findViewById(R.id.etPassword);
+        etUsername = (EditText) root.findViewById(R.id.etUsername);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+        btLostPassword.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                LostPassword();
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return root;
+
+
     }
 
     @Override
@@ -92,19 +84,54 @@ public class fragment_login extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    private void Login()
+    {
+        if(ControleSaisie())
+        {
+            /* TODO: Envoi des informations à l'API et étude du retour */
+        }
+    }
+
+    private void LostPassword()
+    {
+        if(etUsername.getText().toString() == "")
+        {
+            Toast.makeText(getActivity(),"Attention : le champs Username est obligatoire!",Toast.LENGTH_LONG ).show();
+            return;
+        }
+
+        /* TODO: Gérer l'oubli de mot de passe */
+
+    }
+
+    private boolean ControleSaisie()
+    {
+        boolean res = true;
+        String msg = "Problème(s) de saisie:";
+
+        if(etPassword.getText().toString() == "")
+        {
+            res = false;
+            msg += "\n - Le champs Password est obligatoire";
+        }
+
+        if(etUsername.getText().toString() == "")
+        {
+            res = false;
+            msg += "\n - Le champs Username est obligatoire";
+        }
+
+        if(!res)
+        {
+            Toast.makeText(getActivity(),msg,Toast.LENGTH_LONG ).show();
+        }
+
+        return res;
     }
 
 }

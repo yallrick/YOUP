12b2a4f727dfcbe2682ef008e6772f7e73129de1
dmoro33 <moro.damien@gmail.com>
2014-11-14 18:41:31 +0100

    Détail d'événement - Damien Création de l'activity et du fragment détails. Intégration de l'écran.
    Page des commentaires - Damien
    Intégration de l'écran.

---
+++ app/src/main/java/youp/ingesup/com/youp/view/EventActivity.java
@@ -0,0 +1,91 @@
+package youp.ingesup.com.youp.view;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import youp.ingesup.com.youp.R;
+import youp.ingesup.com.youp.adapter.TabsEventAdapter;
+import youp.ingesup.com.youp.view.fragment.CommentsFragment;
+import youp.ingesup.com.youp.view.fragment.DetailsFragment;
+
+import static android.app.ActionBar.TabListener;
+
+/**
+ * Created by Damiano on 14/11/2014.
+ */
+public class EventActivity extends FragmentActivity implements TabListener , DetailsFragment.OnFragmentInteractionListener, CommentsFragment.OnFragmentInteractionListener{
+    private ViewPager viewPager;
+    private TabsEventAdapter mAdapter;
+    private ActionBar actionBar;
+
+    // Tab titles
+    private String[] tabs = { "Details", "Comments"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        actionBar = getActionBar();
+        mAdapter = new TabsEventAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+
+        /**
+         * on swiping the viewpager make respective tab selected
+         * */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/youp/ingesup/com/youp/view/fragment/CommentsFragment.java
@@ -0,0 +1,56 @@
+package youp.ingesup.com.youp.view.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import youp.ingesup.com.youp.R;
+
+/**
+ * Created by Damiano on 14/11/2014.
+ */
+public class CommentsFragment extends Fragment{
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_comments, container, false);
+
+
+        // Inflate the layout for this fragment
+        return root;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(Uri uri);
+    }
+}
+
